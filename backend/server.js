5952c1e3d85f366500ca977ddc39648514fa157d
seb4594 <65371156+seb4594@users.noreply.github.com>
2023-01-26 19:39:44 -0600

    Basic express setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log("server")
+// Configuring .env file
+const dotenv = require('dotenv').config()
+
+// Importing and setting up basic express
+const express = require('express')
+// Port will try .env port and will default to 8000
+const PORT = process.env.PORT || 8000
+const app = express()
+
+
+app.get('/', (req, res) => {
+    res.send('hey')
+ })
+
+// Serving express on port 5000
+app.listen(PORT, () => { console.log('Pinged') })
