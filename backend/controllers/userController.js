cdfde8cee57cf0e154bfad101cd0f4da272017e4
seb4594 <65371156+seb4594@users.noreply.github.com>
2023-01-26 20:34:50 -0600

    Created and connected mongoose schema

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler')
-
+const bcrypt = require('bcryptjs')
+const User = require('../models/userModel')
 // @desc Register a new user
 // @route /api/users
 // @access Public 
@@ -10,7 +11,34 @@ const registerUser = asyncHandler(async(req, res) => {
         throw new Error("Please Include All Fields")
     }
 
-    res.send('Register Route')
+    // Find if user exists
+    const userExists = await User.findOne({ email })
+    
+    if (userExists) { 
+        res.status(400)
+        throw new Error("User Already Exists")
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    // Create User
+    const user = await User.create({
+        name, email, password: hashedPassword
+    })
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        })
+    }
+    else { 
+        res.status(400)
+        throw new error('Invalid user data')
+    }
+
 })
 
 
