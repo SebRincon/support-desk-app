a8c1707ed1bdaba857523ce5f25e021e93dd1abe
seb4594 <65371156+seb4594@users.noreply.github.com>
2023-01-26 20:08:16 -0600

    Installed an asyncHandler lib for promises

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,9 @@
+const asyncHandler = require('express-async-handler')
+
 // @desc Register a new user
 // @route /api/users
 // @access Public 
-const registerUser = (req, res) => { 
+const registerUser = asyncHandler(async(req, res) => { 
     const { name, email, password } = req.body
     if (!name || !email || !password) { 
         res.status(400)
@@ -9,15 +11,15 @@ const registerUser = (req, res) => {
     }
 
     res.send('Register Route')
-}
+})
 
 
 // @desc Login a new user
 // @route /api/users/login
 // @access Public 
-const loginUser = (req, res) => { 
+const loginUser = asyncHandler(async(req, res) => { 
     res.send('login Route')
-}
+})
 
 module.exports = {
     registerUser,
