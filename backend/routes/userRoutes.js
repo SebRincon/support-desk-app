35a9fd3c26a46f1775ba073ff9ee55802aa778b3
seb4594 <65371156+seb4594@users.noreply.github.com>
2023-01-26 19:47:43 -0600

    Created and conntected a function controller for apis

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const { registerUser, loginUser} = require('../controllers/userController')
 
-router.post('/', (req, res) => {
-    res.send('Register Route')
-})
+router.post('/', registerUser)
 
-router.post('/login', (req, res) => {
-    res.send('login Route')
-})
+router.post('/login', loginUser)
 
 module.exports = router
