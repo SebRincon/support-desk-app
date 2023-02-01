f98a9cd1003908d630609485923111def5a06077
seb4594 <65371156+seb4594@users.noreply.github.com>
2023-01-31 19:31:20 -0600

    Update ticket by id handler

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -66,6 +66,39 @@ const deleteTicket = asyncHandler(async (req, res) => {
   await ticket.remove();
   res.status(200).json({ success: true });
 });
+
+// @desc Delete user ticket
+// @route PUT /api/tickets/:id
+// @access Private
+const updateTicket = asyncHandler(async (req, res) => {
+  // Get user using the id in the JWT
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found ");
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+  if (!ticket) {
+    res.status(404);
+    throw new Error("Ticket not found");
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("Not Authorized");
+  }
+
+  const updatedTicket = await Ticket.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+
+  res.status(200).json(updatedTicket);
+});
+
+// @desc Create user ticket
 // @route POST /api/tickets
 // @access Private
 const createTicket = asyncHandler(async (req, res) => {
