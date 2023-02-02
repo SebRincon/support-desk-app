16dea31e08f44aa7d2f5b9a2a6a91d85e36df468
seb4594 <65371156+seb4594@users.noreply.github.com>
2023-02-01 19:55:04 -0600

    Handled the logic functions for creating the tickets

--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -10,15 +10,49 @@ const initialState = {
   message: "",
 };
 
+// Create new ticket
+export const createTicket = createAsyncThunk(
+  "tickets/create",
+  async (ticketData, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      return await ticketService.createTicket(ticketData, token);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
 export const ticketSlice = createSlice({
   name: "ticket",
   initialState,
 
   reducers: {
-    reset: (state) => initialState,
+    reset: (state) => {
+      state.ticket = [];
+      state.ticket = {};
+      state.isError = false;
+      state.isSuccess = false;
+      state.isLoading = false;
+      state.message = "";
+    },
   },
 
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createTicket.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createTicket.fulfilled, (state) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+      })
+      .addCase(createTicket.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      });
+  },
 });
 
 export const { reset } = ticketSlice.actions;
--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -1,19 +1,41 @@
-import { set } from "mongoose";
+import { toast } from "react-toastify";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
+import { useNavigate } from "react-router-dom";
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { createTicket } from "../features/tickets/ticketSlice";
 
 function NewTicket() {
   const { user } = useSelector((state) => state.auth);
+  const { isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.ticket
+  );
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [name] = useState(user.name);
   const [email] = useState(user.email);
   const [product, setProduct] = useState("iPhone");
   const [description, setDescription] = useState("");
+
   const onSubmit = (e) => {
     e.preventDefault();
+    dispatch(createTicket({ product, description }))
+      .unwrap()
+      .then(() => {
+        // We got a good response so navigate the user
+        navigate("/tickets");
+        toast.success("New ticket created!");
+      })
+      .catch(toast.error);
   };
 
+  if (isLoading) {
+    return <Spinner />;
+  }
   return (
     <>
+      <BackButton url={"/"} />
       <section className="heading">
         <h1>Create New Ticket</h1>
         <p>Please Fill The Form Below</p>
